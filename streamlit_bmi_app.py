7d49532b866f07535508f23d759018e72b71d5ac
Bhavya Dua <78466683+Bhavya2120@users.noreply.github.com>
2022-12-24 00:48:52 +0530

    Update streamlit_bmi_app.py

--- streamlit_bmi_app.py
+++ streamlit_bmi_app.py
@@ -3,10 +3,8 @@ import numpy as np
 st.write("""
 #BMI Calculator""")
 st.header('User Input Parameters')
-height=1.00
-weight=40.00
-height=st.number_input('Enter the height in metres')
-weight=st.number_input('Enter the weight in kilograms')
+height=st.number_input('Enter the height in metres',value=1.00)
+weight=st.number_input('Enter the weight in kilograms',value=40)
 #Find a way to accept different input - cm/feet instead of metres
 height=height**2
 bmi=weight/height
